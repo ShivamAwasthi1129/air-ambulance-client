5391f79eee158c2ed7e37d2570f9c502d68215cc
akkinet <akashsharma.zak@gmail.com>
2025-03-12 10:36:44 +0530

    get user api by email

---
+++ src/app/api/user/[email]/route.js
@@ -0,0 +1,39 @@
+import User from "@/app/models/User";
+import { connectToDatabase } from "@/config/mongo";
+import { NextResponse } from "next/server";
+
+export const GET = async (req, { params }) => {
+  try {
+    const text = (await params).email;
+
+    if (!text || text.length < 4) {
+      return NextResponse.json({ error: "Search text must be at least 3 characters long." }, { status: 400 });
+    }
+
+    await connectToDatabase();
+
+    const users = await User.aggregate([
+      {
+        $addFields: {
+          emailUsername: { $arrayElemAt: [{ $split: ["$email", "@"] }, 0] }
+        }
+      },
+      {
+        $match: {
+          emailUsername: { $regex: new RegExp(`${text}`, "i") }
+        }
+      },
+      {
+        $project: {
+          name: 1,
+          email: 1,
+          phone: 1
+        }
+      }
+    ]);
+
+    return NextResponse.json(users);
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+};
