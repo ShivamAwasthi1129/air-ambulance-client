25c34699efd1d6fae81422b1036fb42a95ec80da
Shivam awasthi <90971550+ShivamAwasthi1129@users.noreply.github.com>
2025-09-01 10:16:05 +0530

    minro changes

--- src/app/travelHistory/page.jsx
+++ src/app/travelHistory/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import NavBar from "../components/Navbar";
+// import NavBar from "../components/Navbar";
 import {
   AiOutlineCheckCircle,
   AiOutlineEdit,
